4cc0160f3cf5e7fd324ea1b9beaaa50d620a4f6f
Juan Agudelo <jagudelo@wearegap.com>
2024-08-16 18:47:58 -0500

    Remove the unused SortableList class

--- Application/EdFi.Ods.AdminApi/Infrastructure/Extensions/QueryExtensions.cs
+++ Application/EdFi.Ods.AdminApi/Infrastructure/Extensions/QueryExtensions.cs
@@ -13,50 +13,6 @@ namespace EdFi.Ods.AdminApi.Infrastructure.Extensions
     public static class QueryExtensions
     {
         /// <summary>
-        /// Custom function for sorting.
-        /// We initially try to fix by orderBy. If this column does not exist, we try to sort by orderByDefault.
-        /// </summary>
-        /// <typeparam name="T">Any entity from the model</typeparam>
-        /// <param name="source"></param>
-        /// <param name="orderBy">Try to fix by this column</param>
-        /// <param name="orderByDefault">In case orderBy column does not exist, we sort by this column</param>
-        /// <param name="descending">asc or desc</param>
-        /// <returns></returns>
-        public static IQueryable<T> OrderByColumn<T>(this IQueryable<T> source, string orderBy, string orderByDefault, bool descending = false)
-        {
-            try
-            {
-                var type = typeof(T);
-                var property = type.GetProperty(orderBy, BindingFlags.IgnoreCase | BindingFlags.Public | BindingFlags.Instance);
-                if (property == null)
-                    property = type.GetProperty(orderByDefault, BindingFlags.IgnoreCase | BindingFlags.Public | BindingFlags.Instance);
-
-                if (property != null)
-                {
-                    var parameter = Expression.Parameter(type, "p");
-                    var propertyAccess = Expression.MakeMemberAccess(parameter, property);
-                    var orderByExp = Expression.Lambda(propertyAccess, parameter);
-
-                    var resultExp = Expression.Call(
-                        typeof(Queryable),
-                        descending ? "OrderByDescending" : "OrderBy",
-                        new Type[] { type, property.PropertyType },
-                        source.Expression,
-                        Expression.Quote(orderByExp)
-                    );
-
-                    return source.Provider.CreateQuery<T>(resultExp);
-                }
-                return source;
-            }
-            catch (Exception)
-            {
-                /// If this throws an exception simply don't sort.
-                return source;
-            }
-        }
-
-        /// <summary>
         /// Ordering the IQueryable base in the expression
         /// </summary>
         /// <typeparam name="T">Entity type</typeparam>
