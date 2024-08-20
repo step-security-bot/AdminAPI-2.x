74c4cb6fbd1b99947e18876abea7b19f7bde8e36
Juan Agudelo <jagudelo@wearegap.com>
2024-08-20 11:40:23 -0500

    Fix the orderby in vendors to include contactname, contactemail and nameprefixes

--- Application/EdFi.Ods.AdminApi/Infrastructure/Database/Queries/GetVendorsQuery.cs
+++ Application/EdFi.Ods.AdminApi/Infrastructure/Database/Queries/GetVendorsQuery.cs
@@ -4,6 +4,7 @@
 // See the LICENSE and NOTICES files in the project root for more information.
 
 using System.Linq.Expressions;
+using System.Numerics;
 using EdFi.Admin.DataAccess.Contexts;
 using EdFi.Admin.DataAccess.Models;
 using EdFi.Ods.AdminApi.Helpers;
@@ -30,6 +31,13 @@ public class GetVendorsQuery : IGetVendorsQuery
         (StringComparer.OrdinalIgnoreCase)
     {
         { "company", x => x.VendorName },
+#pragma warning disable CS8602 // Dereference of a possibly null reference.
+        { "contactName", x => x.Users.FirstOrDefault().FullName },
+#pragma warning restore CS8602 // Dereference of a possibly null reference.
+#pragma warning disable CS8602 // Dereference of a possibly null reference.
+        { "contactEmail", x => x.Users.FirstOrDefault().Email },
+#pragma warning restore CS8602 // Dereference of a possibly null reference.
+        { "namespacePrefixes", x => string.Join(",", x.VendorNamespacePrefixes.Select(p=> p.NamespacePrefix)) },
         { "id", x => x.VendorId }
     };
     public GetVendorsQuery(IUsersContext context, IOptions<AppSettings> options)
@@ -57,11 +65,6 @@ public class GetVendorsQuery : IGetVendorsQuery
         Expression<Func<Vendor, object>> columnToOrderBy = _orderByColumnVendors.GetColumnToOrderBy(commonQueryParams.OrderBy);
 
         return _context.Vendors
-            .Where(c => id == null || id < 1 || c.VendorId == id)
-            .Where(c => company == null || c.VendorName == company)
-            .Where(c => c.VendorNamespacePrefixes.Any(v => namespacePrefixes == null || v.NamespacePrefix == namespacePrefixes))
-            .Where(c => c.Users.Any(u => contactName == null || u.FullName == contactName))
-            .Where(c => c.Users.Any(u => contactEmailAddress == null || u.Email == contactEmailAddress))
             .Include(v => v.Applications)
                 .ThenInclude(a => a.Profiles)
             .Include(v => v.Applications)
@@ -71,6 +74,11 @@ public class GetVendorsQuery : IGetVendorsQuery
             .Include(v => v.Users)
             .Include(v => v.VendorNamespacePrefixes)
             .Where(v => !VendorExtensions.ReservedNames.Contains(v.VendorName.Trim()))
+            .Where(c => id == null || id < 1 || c.VendorId == id)
+            .Where(c => company == null || c.VendorName == company)
+            .Where(c => c.VendorNamespacePrefixes.Any(v => namespacePrefixes == null || v.NamespacePrefix == namespacePrefixes))
+            .Where(c => c.Users.Any(u => contactName == null || u.FullName == contactName))
+            .Where(c => c.Users.Any(u => contactEmailAddress == null || u.Email == contactEmailAddress))
             .OrderByColumn(columnToOrderBy, commonQueryParams.IsDescending)
             .Paginate(commonQueryParams.Offset, commonQueryParams.Limit, _options)
             .ToList();
